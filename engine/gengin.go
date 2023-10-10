4889ba2ecbff1107d4e2c9d9457372b6b48573ab
xzh1111 <ermeiyao@outlook.com>
2023-10-10 17:06:45 +0800

    添加apiOuters

--- engine/gengin.go
+++ engine/gengin.go
@@ -4,12 +4,48 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/cookedsteak/gengine/engine"
 )
 
 var genginPoolMap map[string]*engine.GenginePool = make(map[string]*engine.GenginePool)
+
+var ApiOuters map[string]interface{} = make(map[string]interface{})
+
+func AddApiOuters(apiOuters map[string]interface{}) error {
+	for k, v := range apiOuters {
+		ApiOuters[k] = v
+	}
+	return nil
+}
+
+func AddApiOuter(name string, api interface{}) error {
+	ApiOuters[name] = api
+	return nil
+}
+
+func init() {
+	apiOuters := map[string]interface{}{
+		"Println":        fmt.Println,
+		"Now":            time.Now(),
+		"StringContains": strings.Contains,
+		"Abs":            math.Abs,
+		"MathLog":        math.Log,
+		"Log10":          math.Log10,
+		"Log1p":          math.Log1p,
+		"Log2":           math.Log2,
+		"Mod":            math.Mod,
+		"Pow":            math.Pow,
+		"Pow10":          math.Pow10,
+		"Round":          math.Round,
+	}
+
+	AddApiOuters(apiOuters)
+}
 
 func LoadGenginRules() error {
 	ruleFiles, err := loadRuleFiles(*rule_dir, ".erl")
@@ -31,7 +67,7 @@ func LoadGenginRules() error {
 			log.Printf("genginPoolMap create for %s", ruleFile.FileName)
 		}
 		mu.Lock()
-		pool, err := engine.NewGenginePool(2, 100, 1, string(content), nil)
+		pool, err := engine.NewGenginePool(2, 100, 1, string(content), ApiOuters)
 		if err != nil {
 			panic(fmt.Sprintf("init gengine failed, err:%+v", err))
 		}
